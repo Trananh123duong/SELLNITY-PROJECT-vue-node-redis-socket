7a41a7ecba97de864ec98a6eeb5cd082cd138b88
dev05 <dev05@dev.co-mit.com>
2026-03-31 14:23:26 +0700

    setup source FE

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+  baseURL: 'http://localhost:3000/api',
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+apiClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+
+  return config
+})
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => Promise.reject(error)
+)
+
+export default apiClient
